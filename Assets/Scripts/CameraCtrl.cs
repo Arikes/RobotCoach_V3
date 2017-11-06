becef8f0e1b3d8463d2df86d2e206b9757343ed7
liangk <916356609@qq.com>
2017-11-06 16:47:06 +0800

    Signed-off-by: liangk <916356609@qq.com> 添加相机控制，相机可以跟随物体或者定位到某一个位置

---
+++ Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    public Camera mMainCamera;
+    public GameObject mCarObject;
+    public static CameraCtrl instance;
+    
+    //相对于车的相对位置，初始
+    private Vector3 mOldPos;
+    //相对于车的相对位置，目标位置
+    private Vector3 mNewPos;
+    //相机定位方式,1-直线;2-抛物线
+    private int mFlyType;
+
+    void Start()
+    {
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    /*
+     * dstPos:目标位置
+     * dir:相机朝向
+     * type:是否相对于汽车,1:是,0:否
+     */
+    public bool FlyTo(Vector3 dstPos,Vector3 dir,int type)
+    {
+        mFlyType = type;
+        if (mFlyType == 1)
+        {
+            //相对于车的位置
+            mMainCamera.transform.parent = mCarObject.transform;
+            mMainCamera.transform.position = dstPos;
+        } else if (mFlyType == 0)
+        {
+            //绝对坐标
+            //mMainCamera.transform.position = dstPos;
+            mMainCamera.transform.parent = null;
+            mMainCamera.transform.parent = mCarObject.transform;
+        }
+        return true;
+    }
+
+}
--- Assets/Scripts/CarControler.cs
+++ Assets/Scripts/CarControler.cs
@@ -4,43 +4,86 @@ using UnityEngine;
 
 public class CarControler : MonoBehaviour {
     public GameObject mCarObject;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public static CarControler instance;
+
+    private CameraCtrl mMainCamera;
+    // Use this for initialization
+    void Start () {
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void SetCarPose(Vector3 position)
+    {
+        mCarObject.transform.position = position;
+    }
+
+    public Vector3 GetCarPos()
+    {
+        return mCarObject.transform.position;
+    }
+
+    public void ResetCarPose()
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
     {
+        QuickKeyCheck();
+        KeyControl();
+
+        //模型贴地检测处理
+        //Vector3 dstPos = mCarObject.transform.position;
+        //dstPos.y += 50;
+
+        //Vector3 dir = dstPos - mCarObject.transform.position;
+        //Debug.Log(dir);
+        //Ray ray = new Ray(mCarObject.transform.position, -dir * 9999);
+        //RaycastHit hitInfo;
+        //if (Physics.Raycast(ray, out hitInfo, 9999f))
+        //{
+        //    Debug.Log("-----");
+        //}
+    }
+
+    //快捷键处理函数
+    private void QuickKeyCheck()
+    {
+        //快捷键
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            CameraCtrl.instance.FlyTo(new Vector3(0, 150, 0),new Vector3(0.0f, -1.0f, 0.0f), 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.F2))
+        {
+            CameraCtrl.instance.FlyTo(new Vector3(0, 60, 0), new Vector3(0.0f, -1.0f, 0.0f), 1);
+        }
+    }
+
+    //键盘控制汽车
+    private void KeyControl()
+    {
+        //车辆控制
         if (Input.GetKey(KeyCode.W))
         {
-            //Debug.Log("W按键");
             mCarObject.transform.Translate(Vector3.forward);
-        } else if (Input.GetKey(KeyCode.S))
+        }
+        else if (Input.GetKey(KeyCode.S))
         {
-            //Debug.Log("S按键");
             mCarObject.transform.Translate(Vector3.back);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            //Debug.Log("A按键");
             mCarObject.transform.Translate(Vector3.left);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            //Debug.Log("D按键");
             mCarObject.transform.Translate(Vector3.right);
-        }
-
-        Vector3 dstPos = mCarObject.transform.position;
-        dstPos.y += 50;
-
-        Vector3 dir = dstPos - mCarObject.transform.position;
-        Ray ray = new Ray(mCarObject.transform.position, -dir * 9999);
-        RaycastHit hitInfo;
-        if (Physics.Raycast(ray, out hitInfo, 9999f))
-        {
-            Debug.Log("-----");
         }
     }
 }
